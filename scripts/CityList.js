3ef84af4e00f261e140d66daeca47f831f09b238
Sylvia T <sylviarosemarie@gmail.com>
2023-08-11 16:16:32 -0500

    made function to pop the extra walker and store it to a global variable to be used by the click event while not being displayed to the page

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,18 +1,34 @@
 import { getWalkers } from "./database.js";
 
 const walkers = getWalkers();
+let dupeWalker = "";
+let sdWalkers = [];
 
 document.addEventListener("click", (clickEvt) => {
   const cityTarget = clickEvt.target;
   if (cityTarget.dataset.type === "city") {
-    window.alert(`${cityTarget.dataset.walkername} is servicing this city.`);
+    if (cityTarget.innerText === "San Diego" && dupeWalker != "") {
+      sdWalkers.push(dupeWalker.name);
+      sdWalkers.push(cityTarget.dataset.walkername);
+      window.alert(
+        `${sdWalkers[0]} and ${sdWalkers[1]} are servicing this city.`
+      );
+    } else {
+      window.alert(`${cityTarget.dataset.walkername} is servicing this city.`);
+    }
   }
 });
+
+const getCleanArray = (newWalkerArray) => {
+  dupeWalker = walkers.pop();
+  return newWalkerArray;
+};
+const newWalkerArray = getCleanArray(walkers);
 
 export const CityList = () => {
   let citiesHTML = "<ol>";
 
-  for (const walker of walkers) {
+  for (const walker of newWalkerArray) {
     citiesHTML += `<li data-type="city" data-walkername="${walker.name}">${walker.city}</li>`;
   }
 
